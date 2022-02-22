f7ea8e3a475dde8e65059e65d391e07732f6064f
Katie McCauley <katiemccau02@gmail.com>
2022-02-21 20:54:00 -0700

    commit

--- app/Models/Multiple.js
+++ app/Models/Multiple.js
@@ -5,12 +5,18 @@ export class Multiple {
     this.id = generateId()
     this.question = data.question
     this.answer = data.correct_answer
+    this.wrong = data.inncorrect_answers
   }
 
   get Template() {
     return `
       <li>${this.question}</li>
-      
+      <button>${this.answer}</button>
       `
   }
+
+  // get Wrong() {
+  //   let answer = [this.answer, ...this.wrong]
+  //   return console.log(answer);
+  // }
 }
